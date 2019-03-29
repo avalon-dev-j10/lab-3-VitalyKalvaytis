8d14b9b0fda4f6ee7ef88e309eba12a7112664c4
VitalyKalvaytis <35275702+VitalyKalvaytis@users.noreply.github.com>
2019-03-30 00:55:43 +0300

    Update BubbleSort.java

--- src/ru/avalon/java/dev/j10/labs/sort/BubbleSort.java
+++ src/ru/avalon/java/dev/j10/labs/sort/BubbleSort.java
@@ -16,7 +16,17 @@ public class BubbleSort implements Sort {
     /**
      * {@inheritDoc}
      */
+    @Override
     public void sort(int[] array) {
+        for (int i = 0; i < array.length-1; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                if(array[i]< array[j]){
+                    int temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }  
+            } 
+        }
         /*
          * TODO(Студент): Реализовать метод sort класса BubbleSort
          */
